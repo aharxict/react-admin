6d8261455db6a99a27b814d121223d41c7fc285e
aharxict <it.ahapx@gmail.com>
2019-03-19 17:29:39 +0200

    Dom elements and action type names refactoring and inserting baseApiUrl.

--- src/Containers/Body.js
+++ src/Containers/Body.js
@@ -8,13 +8,11 @@ class Body extends PureComponent {
   render () {
     return (
       <div>
-        <div>Body top</div>
         <Switch>
           <Route exact path = "/" component={Dashboard}/>
           <Route path = "/users" component={Users}/>
           <Route path = "/stories" component={Stories}/>
         </Switch>
-        <div>Body bottom</div>
       </div>
     );
   }
--- src/Redux/Actions/actionTypes.js
+++ src/Redux/Actions/actionTypes.js
@@ -1,4 +1,4 @@
 // ---------------------------------------------- Dashboard ----------------------------------------------//
-export const DASHBOARD_LOAD_REQUEST = 'DASHBOARD_LOAD_REQUEST';
-export const DASHBOARD_LOAD_SUCCESS = 'DASHBOARD_LOAD_SUCCESS';
-export const DASHBOARD_LOAD_FAILURE = 'DASHBOARD_LOAD_FAILURE';
+export const DASHBOARD_POSTS_LOAD_REQUEST = 'DASHBOARD_POSTS_LOAD_REQUEST';
+export const DASHBOARD_POSTS_LOAD_SUCCESS = 'DASHBOARD_POSTS_LOAD_SUCCESS';
+export const DASHBOARD_POSTS_LOAD_FAILURE = 'DASHBOARD_POSTS_LOAD_FAILURE';

d2f0cc1cb189bb691b0ff2193572dab0ea2b87a9
aharxict <it.ahapx@gmail.com>
2019-03-21 17:32:10 +0200

    Added reducer, actions and implements loading of users list.

--- src/Redux/Actions/actionTypes.js
+++ src/Redux/Actions/actionTypes.js
@@ -2,3 +2,8 @@
 export const DASHBOARD_POSTS_LOAD_REQUEST = 'DASHBOARD_POSTS_LOAD_REQUEST';
 export const DASHBOARD_POSTS_LOAD_SUCCESS = 'DASHBOARD_POSTS_LOAD_SUCCESS';
 export const DASHBOARD_POSTS_LOAD_FAILURE = 'DASHBOARD_POSTS_LOAD_FAILURE';
+
+// ---------------------------------------------- Users list ----------------------------------------------//
+export const USERS_LIST_LOAD_REQUEST = 'USERS_LIST_LOAD_REQUEST';
+export const USERS_LIST_LOAD_SUCCESS = 'USERS_LIST_LOAD_SUCCESS';
+export const USERS_LIST_LOAD_FAILURE = 'USERS_LIST_LOAD_FAILURE';

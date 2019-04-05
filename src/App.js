e892a7f4980481645c74ef4f74432aeba9911275
aharxict <it.ahapx@gmail.com>
2019-04-05 16:43:46 +0300

    Adds fortawesome package.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import mainStore from './Redux/Store/mainStore'
 import './sccs/style.scss';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faTimes, faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faTimes, faCoffee);
 
 class App extends Component {
   render() {
